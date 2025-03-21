7769b5cd370ceedf503372b2cbb569162730cd63
Cynny <c.dafinone@gmail.com>
2025-03-21 20:38:09 +0000

    add icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Footer = () => {
   var year = new Date().getFullYear()
     return (
       <div style={{color:"#333"}}>
       
-        Copyright &copy; {year} Xertz industries.
+        <p>Copyright &copy; {year} Xertz industries.</p>
+        <p>
+        <FontAwesomeIcon icon="fa-brands fa-square-instagram" />
+        <FontAwesomeIcon icon="fa-brands fa-square-whatsapp" />
+        <FontAwesomeIcon icon="fa-brands fa-square-youtube" />
+        <FontAwesomeIcon icon="fa-brands fa-square-envelope" />
+        </p>
       </div>
+      
     );
   };
   
